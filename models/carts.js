de560516af0882cd1c8517f1376851eeb7749095
reshabh27 <reshabhsahu10@gmail.com>
2024-02-26 18:21:33 +0530

    Created cart Model

--- models/carts.js
+++ models/carts.js
@@ -1,19 +1,44 @@
-const mongoose = require('mongoose');
 
-const cartSchema = new mongoose.Schema({
-    products:[
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Product'
+
+
+
+
+module.exports = (sequelize, DataTypes) => {
+    const Cart = sequelize.define("Cart", {
+        products: {
+            type: DataTypes.INTEGER,
+        },
+        quantity: {
+            type: DataTypes.INTEGER
+        },
+        owner: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
         }
-    ],
-    owner : {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: 'User'
-    }
-})
+    })
+    return Cart;
+}
 
-const Cart = mongoose.model('Cart', cartSchema);
 
-module.exports = Cart;
+
+
+
+// const mongoose = require('mongoose');
+
+// const cartSchema = new mongoose.Schema({
+// products:[
+//     {
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: 'Product'
+//     }
+// ],
+// owner : {
+//     type: mongoose.Schema.Types.ObjectId,
+//     required: true,
+//     ref: 'User'
+// }
+// })
+
+// const Cart = mongoose.model('Cart', cartSchema);
+
+// module.exports = Cart;
