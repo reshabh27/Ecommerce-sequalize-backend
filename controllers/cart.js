c245c60d08c4033f171757fd64d25745f490ac90
reshabh27 <reshabhsahu10@gmail.com>
2024-02-28 12:28:17 +0530

    added one more method to get values from related tables: implemented in cartController-handleGetCartOfUser

--- controllers/cart.js
+++ controllers/cart.js
@@ -5,17 +5,30 @@ const Cart = db.cart;
 
 exports.handleGetCartOfUser = async (req, res) => {
     try {
-        let curcart = await User.findByPk(req.user.id,
-            {
-                include: [{
-                    model: Product,
-                    through: { model: Cart, },
-                }],
-            })
-        console.log(curcart);
+        // 1st method to get values of many to many associated tables
+        // let curcart = await User.findByPk(req.user.id,
+        //     {
+        //         include: [{
+        //             model: Product,
+        //             through: { model: Cart, },
+        //         }],
+        //     })
+
+
+        // 2nd method to get values from many to many associated tables.
+        let data = await User.findOne({
+            where: {
+                id: req.user.id
+            }
+        })
+        let curcart = await data.getProducts();
+
+
+        // console.log("data", data);
+        // console.log("curcart", curcart);
         if (!curcart)
             return res.send([]);
-        return res.status(200).send(curcart);
+        return res.status(200).send({ data, cart: curcart });
     } catch (err) {
         console.log(err);
         return res.status(400).send();
@@ -24,22 +37,7 @@ exports.handleGetCartOfUser = async (req, res) => {
 
 
 exports.handleAddProductInCart = async (req, res) => {
-    // const oldProducts = await Cart.findOne({ owner: req.user._id });
-    // // console.log(oldProducts);
-    // if (oldProducts?.products?.includes(req.params.productId)) {
-    //     return res.status(400).send({ error: 'Product is already in the cart' });
-    // }
     try {
-        // if (!oldProducts) {
-        //     const cart = new Cart({
-        //         products: [req.params.productId],
-        //         owner: req.user._id
-        //     });
-        //     await cart.save();
-        // }
-        // else
-        //     await Cart.findByIdAndUpdate(oldProducts._id, { products: [...oldProducts.products, req.params.productId] })
-        // console.log(test);
         const newProduct = await Cart.create({ UserId: req.user.id, ProductId: req.params.productId, quantity: 1 });
         // console.log(newProduct);
         return res.status(200).send({ message: "succesfully added product in cart", newProduct });
