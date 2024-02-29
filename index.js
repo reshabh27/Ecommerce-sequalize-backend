014abb9be727f11a9ad38a19ffeab8ba495804e8
reshabh27 <reshabhsahu10@gmail.com>
2024-02-29 16:41:36 +0530

    Added default router for not defined route error handling

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ app.use('/api/v1/user', userRouter);
 app.use('/api/v1/product', productRouter);
 app.use('/api/v1/cart', cartRouter);
 
+// error handling for undefined routes
+app.all('*', (req, res, next) => {
+    res.status(400).send({ status: 'fail', message: `can't find ${req.originalUrl} on the server!` })
+})
+
 // app.get('/', function (req, res) {
 //     res.send('Hello World')
 // })
